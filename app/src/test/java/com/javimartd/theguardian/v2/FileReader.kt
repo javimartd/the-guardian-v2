a87e15e798d320e129ed89f1e3bdbedcc83fcc90
Javi Martinez <javimartd@gmail.com>
2021-11-18 15:15:38 +0100

    chore: including mock responses and FileRe

---
+++ app/src/test/java/com/javimartd/theguardian/v2/FileReader.kt
@@ -0,0 +1,27 @@
+package com.javimartd.theguardian.v2
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+object FileReader {
+
+    @Throws(IOException::class)
+    fun readFileWithoutNewLineFromResources(fileName: String): String {
+        var inputStream: InputStream? = null
+        try {
+            inputStream = javaClass.classLoader?.getResourceAsStream(fileName)
+            val builder = StringBuilder()
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            var str: String? = reader.readLine()
+            while (str != null) {
+                builder.append(str)
+                str = reader.readLine()
+            }
+            return builder.toString()
+        } finally {
+            inputStream?.close()
+        }
+    }
+}
