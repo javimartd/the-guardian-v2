4c5d16e8ca5523b7ca4fec87426c0b27bbbf5da8
javimartd <javimartd@gmail.com>
2022-11-26 16:03:49 +0100

    tests: including tests for extensions

---
+++ app/src/test/java/com/javimartd/theguardian/v2/ui/extensions/ExtensionsUtilsTest.kt
@@ -0,0 +1,23 @@
+package com.javimartd.theguardian.v2.ui.extensions
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ExtensionsUtilsTest {
+
+    @Test
+    fun `to date`() {
+        val dateLong = "2022-10-11T15:49:25Z".toLong(format = DATE_TIME_API_FORMAT)
+        val dateString = dateLong.toDate()
+        Assert.assertEquals(dateString, "11 oct. 2022")
+    }
+
+    @Test
+    fun `to long`() {
+        val dateLong = "2022-10-11T15:49:25Z".toLong(format = DATE_TIME_API_FORMAT)
+        Assert.assertEquals(dateLong, 1665496165000)
+    }
+}
---
+++ app/src/test/java/com/javimartd/theguardian/v2/utils/TestDispatcherRule.kt
@@ -0,0 +1,23 @@
+package com.javimartd.theguardian.v2.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class TestDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+): TestWatcher() {
+
+    // Do something before starting your test
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    // Do something after finishing your test
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
