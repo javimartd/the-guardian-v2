6a0a8fa589876ced1ec3baeb3ce84e4775bba86d
javimartd <javimartd@gmail.com>
2023-01-24 20:24:47 +0100

    tests: updating tests for ExtensionsUtilsTest.kt

--- app/src/test/java/com/javimartd/theguardian/v2/ui/extensions/ExtensionsUtilsTest.kt
+++ app/src/test/java/com/javimartd/theguardian/v2/ui/extensions/ExtensionsUtilsTest.kt
@@ -8,7 +8,7 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class ExtensionsUtilsTest {
 
-    /*@Test
+    @Test
     fun `to date`() {
         val dateLong = "2022-10-11T15:49:25Z".toLong(format = DATE_TIME_API_FORMAT)
         val dateString = dateLong.toDate()
@@ -19,5 +19,5 @@ class ExtensionsUtilsTest {
     fun `to long`() {
         val dateLong = "2022-10-11T15:49:25Z".toLong(format = DATE_TIME_API_FORMAT)
         Assert.assertEquals(dateLong, 1665496165000)
-    }*/
+    }
 }
