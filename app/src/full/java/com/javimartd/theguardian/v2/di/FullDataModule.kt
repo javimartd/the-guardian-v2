485484e5fbe1292d075783adc7773057b31550de
javier.diaz <javier.diaz@levelshoes.com>
2024-01-04 20:09:24 +0400

    fix: FullDataModule.kt

--- app/src/full/java/com/javimartd/theguardian/v2/di/FullDataModule.kt
+++ app/src/full/java/com/javimartd/theguardian/v2/di/FullDataModule.kt
@@ -1,16 +1,15 @@
 package com.javimartd.theguardian.v2.di
 
-import com.javimartd.theguardian.v2.data.datasources.ErrorHandler
 import com.javimartd.theguardian.v2.data.datasources.CacheDataSource
 import com.javimartd.theguardian.v2.data.datasources.DiskDataSource
+import com.javimartd.theguardian.v2.data.datasources.ErrorHandler
 import com.javimartd.theguardian.v2.data.datasources.RemoteDataSource
 import com.javimartd.theguardian.v2.data.datasources.cache.CacheDataSourceImpl
-import com.javimartd.theguardian.v2.data.datasources.disk.db.news.DiskDataSourceImpl
 import com.javimartd.theguardian.v2.data.datasources.disk.db.AppDatabase
+import com.javimartd.theguardian.v2.data.datasources.disk.db.news.DiskDataSourceImpl
 import com.javimartd.theguardian.v2.data.datasources.remote.*
 import com.javimartd.theguardian.v2.data.datasources.remote.common.RemoteErrorHandlerImpl
 import com.javimartd.theguardian.v2.data.datasources.remote.news.NewsApiService
-import com.javimartd.theguardian.v2.data.datasources.remote.news.RemoteDataSourceImpl
 import com.javimartd.theguardian.v2.data.datasources.remote.news.mapper.NewsRemoteMapper
 import dagger.Module
 import dagger.Provides
